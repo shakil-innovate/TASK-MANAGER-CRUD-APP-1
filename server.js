e5b856af011b0b8bd197be1a6e087d94345cf40e
SHAKIL AHMED <shakil.innovate@gmail.com>
2026-05-05 08:47:17 +0600

    merge route and server

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express=require("express");
 
-// routes
-const routes=require("./Routes/CRUD_Routes");
+const {addTodo,getAllTask,getTaskById,getTaskBySearch,getTaskByFilter,updateTask,deleteTask}=require("./ALL_FUNCTION/functions");
 
+//copy of express
 const app=express();
 const Port=3000;
 
 app.use(express.json());
 
-
+//homeroute
 app.get("/",(req,res)=>{
     res.send("Hello,Start");
 });
 
 
-app.use("/tasks",routes);
+app.get("/",getAllTask);
+app.post("/",addTodo);
+app.get("/search",getTaskBySearch);
+app.get("/filter/:status",getTaskByFilter);
+app.get("/:id",getTaskById);
+app.put("/:id",updateTask);
+app.delete("/:id",deleteTask);
+
 
 app.listen(Port,()=>{
     console.log(`Server running on http://localhost:${Port}`);
